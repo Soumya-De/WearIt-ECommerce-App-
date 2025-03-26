311a54634b7f01c46323c1ef3e497b83078d92d0
Soumya-De <soumyadey820@gmail.com>
2025-03-26 20:27:07 +0530

    signUp screen created

---
+++ app/src/main/java/com/example/ecommerceapp/presentation/signUpScreen.kt
@@ -0,0 +1,18 @@
+package com.example.e_commerceapp.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun SignUpScreen(){
+    val context = LocalContext.current
+    var firstname by remember { mutableStateOf("") }
+    var lastname by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    var phoneNumber by remember { mutableStateOf("") }
+}
