5a4bb5dbf7f9ed1b6dfc97398e1ef24608ecdd93
Soumya-De <soumyadey820@gmail.com>
2025-04-01 09:40:02 +0530

    Initialize Dependency Injection

--- app/src/main/java/com/example/ecommerceapp/MainActivity.kt
+++ app/src/main/java/com/example/ecommerceapp/MainActivity.kt
@@ -6,8 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.ecommerceapp.ui.theme.ECommerceAppTheme
 import com.example.ecommerceapp.presentation.LoginScreenUI
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
