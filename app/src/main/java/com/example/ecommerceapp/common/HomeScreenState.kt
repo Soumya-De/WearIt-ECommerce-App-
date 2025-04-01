722fa3c646a5d7f3266293a77825eb3cec8dc0cb
Soumya-De <soumyadey820@gmail.com>
2025-04-01 09:41:52 +0530

    creating different result state as per requirement of database

---
+++ app/src/main/java/com/example/ecommerceapp/common/HomeScreenState.kt
@@ -0,0 +1,13 @@
+package com.example.ecommerceapp.common
+
+import com.example.ecommerceapp.domain.models.BannerDataModels
+import com.example.ecommerceapp.domain.models.CategoryDataModels
+import com.example.ecommerceapp.domain.models.ProductDataModels
+
+data class HomeScreenState(
+    val isLoading: Boolean = true,
+    val errorMessages: String? = null,
+    val categories: List<CategoryDataModels>? = null,
+    val products: List<ProductDataModels>? = null,
+    val banner: List<BannerDataModels>? = null,
+)
---
+++ app/src/main/java/com/example/ecommerceapp/common/constant.kt
@@ -0,0 +1,6 @@
+package com.example.ecommerceapp.common
+
+const val USER_COLLECTION = "users"
+const val PRODUCTS_COLLECTION = "products"
+const val ADD_TO_FAV = "add_to_fav"
+const val ADD_TO_CART = "add_to_cart"
