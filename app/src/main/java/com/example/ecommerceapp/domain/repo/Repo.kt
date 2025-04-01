4a14fb5c22053f63cfe9950b460b819dc9c43fb4
Soumya-De <soumyadey820@gmail.com>
2025-04-01 09:54:32 +0530

    Created Repo interface to define data operations
    Repo interface defines what data operation are needed. It connect Presentation layer with Data layer via Domain.

---
+++ app/src/main/java/com/example/ecommerceapp/domain/repo/Repo.kt
@@ -0,0 +1,33 @@
+package com.example.ecommerceapp.domain.repo
+
+import android.net.Uri
+import com.example.ecommerceapp.common.ResultState
+import com.example.ecommerceapp.domain.models.BannerDataModels
+import com.example.ecommerceapp.domain.models.CartDataModels
+import com.example.ecommerceapp.domain.models.CategoryDataModels
+import com.example.ecommerceapp.domain.models.ProductDataModels
+import com.example.ecommerceapp.domain.models.UserData
+import com.example.ecommerceapp.domain.models.UserDataParent
+import kotlinx.coroutines.flow.Flow
+
+interface Repo {
+    fun RegisterUserWithEmailAndPassword(userData: UserData): Flow<ResultState<String>>
+    fun LoginUserWithEmailAndPassword(userData: UserData): Flow<ResultState<String>>
+    fun getUserById(uid: String): Flow<ResultState<UserDataParent>>
+    fun updateUserData(userDataParent: UserDataParent): Flow<ResultState<String>>
+    fun userProfileImage(uri: Uri): Flow<ResultState<String>>
+    fun getCategoriesInLimited(): Flow<ResultState<List<CategoryDataModels>>>
+    fun getProductsInLimited(): Flow<ResultState<List<ProductDataModels>>>
+    fun getAllProducts(): Flow<ResultState<List<ProductDataModels>>>
+    fun getProductById(productId: String): Flow<ResultState<ProductDataModels>>
+    fun addToCart(cartDataModels: CartDataModels): Flow<ResultState<String>>
+    fun addTOFav(productDataModels: ProductDataModels): Flow<ResultState<String>>
+    fun getAllFav(): Flow<ResultState<List<ProductDataModels>>>
+    fun getCart(): Flow<ResultState<List<CartDataModels>>>
+    fun getAllCategories(): Flow<ResultState<List<CategoryDataModels>>>
+    fun getCheckout(productId:String): Flow<ResultState<ProductDataModels>>
+    fun getBanner(): Flow<ResultState<List<BannerDataModels>>>
+    fun getSpecificCategoryItems(categoryName: String): Flow<ResultState<List<ProductDataModels>>>
+    fun getAllSuggestedProducts(): Flow<ResultState<List<ProductDataModels>>>
+}
+
